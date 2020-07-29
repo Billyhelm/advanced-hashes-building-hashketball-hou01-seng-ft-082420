d9edec97f22ac30cbcc92011457bc766b810362e
Billy Helm <billyhelm88@gmail.com>
2020-07-29 18:44:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,24 @@
 def game_hash
-  hash = {:home =>[
-    :team_name,
-    :colors, 
-    :players],
+  hash = {:home =>{
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"], 
+    :players => [
+      {:alan_anderson =>{
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebound => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+        }},
+      {:reggie_evans,
+      {:brook_lopez,
+      {:mason_plumlee,
+      {:jason_terry]},
   :away =>[
-    ]
+    :team_name,
+    :colors,
+    :players]}
 end 
