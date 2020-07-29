cdc644c1d86bd81325c7e6b17a4a189de5f35221
Billy Helm <billyhelm88@gmail.com>
2020-07-29 19:06:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
         :slam_dunks => 1
         }},
       {:reggie_evans => { 
-        :player_name => "Reggie Evans"
+        :player_name => "Reggie Evans",
         :number => 30,
         :shoe => 14,
         :points => 12,
@@ -26,6 +26,7 @@ def game_hash
         :slam_dunks => 7
         }},        
       {:brook_lopez => {
+        :player_name => "Brook Lopez",
         :number => 11,
         :shoe => 17,
         :points => 17,
@@ -36,6 +37,7 @@ def game_hash
         :slam_dunks => 15
         }},
       {:mason_plumlee => {
+        :player_name => "Mason Plumlee",
         :number => 1,
         :shoe => 19,
         :points => 26,
@@ -46,6 +48,7 @@ def game_hash
         :slam_dunks => 5
         }},        
       {:jason_terry => {
+        :player_name => "Jason Terry"
         :number => 31,
         :shoe => 15,
         :points => 19,
